2931895a28b7b1f9a22655b98c5ecedef0263df4
Spencer <36741900+Spencer-0003@users.noreply.github.com>
2022-01-04 03:56:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TelemetryBlocker
+Block Microsoft and Google Telemetry on your pc.
+
+## This is only compatible with Windows & Linux
+
+### Features
+1. Block Windows Telemetry (stop Microsoft from collecting data about your pc).
+2. Block Google Telemetry (block various Google trackers).
+3. Block Misc Ads & Trackers (blocks common trackers used on websites for analytics etc).
+
+Endpoints can be found in [here](https://github.com/Spencer-0003/TelemetryBlocker/blob/master/TelemetryBlocker/Endpoints.h)
---
+++ TelemetryBlocker/TelemetryBlocker.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include "Endpoints.h"
+using namespace std;
+
+// Constants
+const string WINDOWS_HOSTS = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+const string LINUX_HOSTS = "/etc/hosts";
+
+// Methods
+void appendFile(string file, string toAppend)
+{
+    ofstream outfile;
+    outfile.open(file, ios_base::app);
+    outfile << toAppend;
+    outfile.close();
+}
+
+void blockMicrosoftTelemetry(string HOSTS, bool windows)
+{
+    appendFile(HOSTS, "\n# TelemetryBlocker (Block Microsoft Telemetry)\n");
+    for (size_t i = 0; i < MicrosoftTelemetryEndpoints.size(); i++)
+        appendFile(HOSTS, "0.0.0.0 " + MicrosoftTelemetryEndpoints[i] + "\n");
+
+    if (windows) {
+        system("sc config \"DiagTrack\" start=disabled");
+        system("sc config \"diagnosticshub.standardcollector.service\" start=disabled");
+        cout << "\nDisabled tracking services" << endl;
+    }
+
+    cout << "Blocked Windows Telemetry\n" << endl;
+}
+
+void blockGoogleTelemetry(string HOSTS) {
+    appendFile(HOSTS, "\n# TelemetryBlocker (Ads & Tracking)\n");
+    for (size_t i = 0; i < GoogleTrackingEndpoints.size(); i++)
+        appendFile(HOSTS, "0.0.0.0 " + GoogleTrackingEndpoints[i] + "\n");
+    cout << "\nBlocked Ads & Tracking Telemetry\n" << endl;
+}
+
+void blockMiscTrackersAndAds(string HOSTS) {
+    appendFile(HOSTS, "\n# TelemetryBlocker (Misc Ads & Tracking)\n");
+    for (size_t i = 0; i < MiscAdsAndTrackingEndpoints.size(); i++)
+        appendFile(HOSTS, "0.0.0.0 " + MiscAdsAndTrackingEndpoints[i] + "\n");
+    cout << "\nBlocked Misc ads & Trackers" << endl;
+}
+
+// Main loop
+int main()
+{
+    bool isWindows = filesystem::exists(WINDOWS_HOSTS); // The bad operating system
+    while (true) {
+        int option;
+        cout << "TelemetryBlocker\n\n1. Block Telemetry (Windows)\n2. Block Google Telemetry (Ads & Tracking)\n3. Block Misc Ads & Trackers \n\nChoose an option: " << endl;
+        cin >> option;
+
+        if (!cin)
+            break;
+
+        switch (option) {
+        case 1:
+            blockMicrosoftTelemetry(isWindows ? WINDOWS_HOSTS : LINUX_HOSTS, isWindows);
+            break;
+        case 2:
+            blockGoogleTelemetry(isWindows ? WINDOWS_HOSTS : LINUX_HOSTS);
+            break;
+        case 3:
+            blockMiscTrackersAndAds(isWindows ? WINDOWS_HOSTS : LINUX_HOSTS);
+            break;
+        default:
+            cout << "Invalid option.\n" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
